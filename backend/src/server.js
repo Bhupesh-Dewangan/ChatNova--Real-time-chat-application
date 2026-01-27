ad84820261a8c28ad25d4510032ddac5e8818a6a
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-01-27 13:27:11 +0530

    online users implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ import messageRoutes from "./routes/message.route.js";
 
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import { app, server } from "./lib/socket.js";
 
 const app = express();
 const __dirname = path.resolve();
@@ -15,7 +16,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(express.json());
+app.use(express.json({ limit: "5mb" })); // req.body
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -30,7 +31,7 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
   connectDB();
 });
