e2878303f0809fdad7530cbbd28b759fe3c3af06
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-02-06 18:20:30 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,8 +21,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
